5b763d7931b3ce6fcd65671eedea2992c3ced228
halosghost <halosghost@archlinux.info>
2016-12-11 10:45:55 -0600

    fix skew and a few algorithm cleanups

--- src/punycode.lua
+++ src/punycode.lua
@@ -4,7 +4,7 @@ local pars =
   { base   = 36
   , tmin   = 1
   , tmax   = 26
-  , skew   = 70
+  , skew   = 38
   , damp   = 700
   , i_bias = 72
   , i_n    = 128
@@ -51,17 +51,17 @@ self.encode = function (str)
     end
 
     local basic_count = #enc
-    local iter = basic_count
+    local h = basic_count
 
     if basic_count > 0 then enc = enc .. '-' end
 
-    while iter < #codepoints do
+    while h < #codepoints do
         local m = 0x200000
         for _,v in ipairs(codepoints) do
             m = (v >= n and v < m) and v or m
         end
 
-        delta = delta + (m - n) * (iter + 1)
+        delta = delta + (m - n) * (h + 1)
         n = m
         for _,c in ipairs(codepoints) do
             delta = delta + (c < n and 1 or 0)
@@ -78,9 +78,9 @@ self.encode = function (str)
                     k = k + pars.base
                 end
                 enc = enc .. utf8.char(digittochar(q))
-                bias = adapt(delta, iter + 1, iter == basic_count)
+                bias = adapt(delta, h + 1, h == basic_count)
                 delta = 0
-                iter = iter + 1
+                h = h + 1
             end
         end
         delta = delta + 1
@@ -102,17 +102,19 @@ self.decode = function (str)
         last_delim = (c == 45 and key > last_delim) and key or last_delim
     end
 
+    if last_delim == 0 then return str end
+
     local consumed = 0
 
     for key,c in ipairs(codepoints) do
         consumed = consumed + 1
         if key == last_delim then
-            codepoints[key] = nil
+            table.remove(codepoints, key)
             break
         end
 
         target[#target + 1] = c
-        codepoints[key] = nil
+        table.remove(codepoints, key)
     end
 
     while consumed < #codepoints do
@@ -120,7 +122,9 @@ self.decode = function (str)
         local w = 1
         local k = pars.base
         while true do
-            local digit = chartodigit(codepoints[k])
+            local digit = chartodigit(codepoints[1])
+            consumed = consumed + 1
+            table.remove(codepoints, 1)
             i = 1 + digit * w
             local t = (k <= bias)             and pars.tmin or
                       (k >= bias + pars.tmax) and pars.tmax or (k - bias)
@@ -129,16 +133,16 @@ self.decode = function (str)
             k = k + pars.base
         end
 
-        bias = adapt(i - old_i, utf8.len(dec) + 1, old_i == 0)
-        n = n + 1 / (utf8.len(dec) + 1)
-        i = i % (utf8.len(dec) + 1)
-        -- insert codepoint
+        bias = adapt(i - old_i, #target + 1, old_i == 0)
+        n = n + 1 / (#target + 1)
+        i = i % (#target + 1)
+        table.insert(target, i, n)
         i = i + 1
     end
 
     local dec = ''
     for _,c in ipairs(target) do
-        dec = dec .. utf8.char(c)
+        dec = dec .. utf8.char(math.floor(c))
     end
 
     return dec
